8e253964d9e971a19d573262d5bcecea67e1202a
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-02-28 00:23:43 +0530

    update

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # titli
 
-```shell
+```bash
+$ sudo apt update --yes
+$ sudo apt upgrade --yes
+
+$ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+$ sudo apt-get install bzip2
+$ bash miniconda.sh -b -p miniconda
+$ echo "export PATH=\$PATH:\$HOME/setups/miniconda/bin" >> ~/.bashrc
+$ source ~/.bashrc
+
+$ conda create -n sih python==3.5 keras
+$ source activate sih
+
 $ python train.py -d "../dataset/flood" -m "../output/trained_model" -l "../output/bin" -p "../output/plot"
+$ python predict.py --image "../tests"
 ```
--- src/train.py
+++ src/train.py
@@ -1,5 +1,23 @@
 from setup import *
 from main import *
+from keras.utils import to_categorical
 
-print(data)
-print(labels)
+# partition the data into training and testing splits using 75% of
+# the data for training and the remaining 25% for testing
+(trainX, testX, trainY, testY) = train_test_split(data,
+	labels, test_size=0.25, random_state=42)
+
+# convert the labels from integers to vectors (for 2-class, binary
+# classification you should use Keras' to_categorical function
+# instead as the scikit-learn's LabelBinarizer will not return a
+# vector)
+lb = to_categorical(lb, num_classes=2, dtype='float32')
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+
+# define the 3072-1024-512-3 architecture using Keras
+model = Sequential()
+model.add(Dense(1024, input_shape=(3072,), activation="sigmoid"))
+model.add(Dense(512, activation="sigmoid"))
+model.add(Dense(len(lb.classes_), activation="softmax"))
+
